825fd11e063a0befaa3d70c9e68f189de00752cb
amit7309830152 <amit7309830152@gmail.com>
2024-04-25 22:37:17 +0530

    a

--- Web demo/node/app.js
+++ Web demo/node/app.js
@@ -1,49 +1,25 @@
-function createSlot(start, end, interval) {
-    const slot = [];
-    slot.push(start)
-    let i = 0
-    while (true) {
-        let s = addInterval(start, interval);
-        console.log(s);
-        slot.push(s);
-        i++
-        start = s;
-        if ((start.split(':')[0] == end.split(':')[0])) {
-            break;
+let obj = {
+    item: {
+        name: 'item1',
+        item: {
+            name: 'item3'
+        }
+    },
+    name: 'amit',
+
+}
+let temp = []
+function print(obj) {
+    for (const key in obj) {
+        if (typeof obj[key] == 'object') {
+            print(obj[key]);
+        }
+        else {
+            temp.push(obj[key])
         }
     }
-    return slot;
-}
-
-function addInterval(time, interval) {
-    let [h, m] = time.split(':')
-    let newM = +m + interval
-    if (newM >= 60) {
-        h = +h + 1;
-        m = newM - 60;
-    } else {
-        m = newM;
-    }
-    h = String(h);
-    m = String(m);
-    console.log('====================================');
-    console.log(h.length);
-    console.log(m.length);
-    console.log('====================================');
-    if (h.length == 1) {
-        h = '0' + String(h)
-    }
-    if (m.length == 1) {
-        m = '0' + String(m)
-    }
-    console.log(`${h}:${m}`);
-    return `${h}:${m}`
-    // return ` ${(h.length == 1) ? '0' + h : h}:${(m.length == 1) ? '0' + m : m}`
-
+    return temp
 }
 
 
-
-
-console.log(createSlot('08:00', '14:00', 30));
-// console.log(addInterval('08:30', 30));
+console.log(print(obj))
