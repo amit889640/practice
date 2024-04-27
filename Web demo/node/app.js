a5eaf6db4f959eb545d4612230338a0e55c94bc3
amit7309830152 <amit7309830152@gmail.com>
2024-04-28 00:37:13 +0530

    push code

--- Web demo/node/app.js
+++ Web demo/node/app.js
@@ -1,9 +1,39 @@
-function fact(n) {
-    if (n == 0) {
-        return 1;
-    } else {
-        return n * fact(n - 1)
-    }
-}
+// const priorityLog = () => {
+//     setTimeout(() => console.log("timeout"), 0);
+//     setImmediate(() => console.log("immediate"));
+//     console.log("I am independent");
+//     process.nextTick(() => console.log("nextTick"));
+// }
+// priorityLog()
 
-console.log(fact(4))
+// I am independent
+// nextTick
+// timeout
+// immediate
+
+
+// pending call()
+// timers function
+// pol
+
+// micro and macro
+
+// function getData1() {
+//     return new Promise((res, rej) => {
+//         setTimeout(() => {
+//             res("data received")
+//         }, 1000);
+//     })
+// }
+
+// async function fetchData() {
+//     const data = await getData();
+// }
+
+// const getdata = async () => {
+//     const data = await getData1();
+//     console.log(data)
+//     console.log("got it");
+// }
+
+// getdata();
--- interview question node js/devnagri.js
+++ interview question node js/devnagri.js
@@ -27,45 +27,45 @@
 
 
 
-{
-    Room_id: number | string,
-        meetingStart: Date,
-            meetingEnd: Date,
-                personDetail: {
-        [
-            {
-                personId: number,
-                personEmail: string,
-                personContact: string,
-            }
+// {
+//     Room_id: number | string,
+//         meetingStart: Date,
+//             meetingEnd: Date,
+//                 personDetail: {
+//         [
+//             {
+//                 personId: number,
+//                 personEmail: string,
+//                 personContact: string,
+//             }
 
-        ]
-    }
-}
-
-
-db.getCollection("meetingDetail").update({ Room_id: 1, personDetail.personId: 2}, { personDetail.$.personEmail: 'amit@gmal.com'})
+//         ]
+//     }
+// }
 
 
+// db.getCollection("meetingDetail").update({ Room_id: 1, personDetail.personId: 2}, { personDetail.$.personEmail: 'amit@gmal.com'})
 
-function pattern(n) {
-    var p = '';
-    for (let i = 0; i <= n; i++) {
-        for (let j = 0; j <= n; j++) {
-            if (i + j == n) {
-                p = p + '*';
-            } else if (i == 0 || i == n) {
-                p = p + '*';
-            } else {
-                p = p + ' ';
-            }
-        }
-        p = p + '\n'
-    }
-    return p;
-}
 
-console.log(pattern(10));
+
+// function pattern(n) {
+//     var p = '';
+//     for (let i = 0; i <= n; i++) {
+//         for (let j = 0; j <= n; j++) {
+//             if (i + j == n) {
+//                 p = p + '*';
+//             } else if (i == 0 || i == n) {
+//                 p = p + '*';
+//             } else {
+//                 p = p + ' ';
+//             }
+//         }
+//         p = p + '\n'
+//     }
+//     return p;
+// }
+
+// console.log(pattern(10));
 
 
 // 00 01 02 03 04
@@ -73,3 +73,8 @@ console.log(pattern(10));
 // 20 21 22 23 24
 // 30 31 32 33 34
 // 40 41 42 43 44
+
+
+
+// Status
+// selected for 1 and 2 round clear
