a46dec89d9d486954ac51e8efeecaac025b3a855
amit7309830152 <amit7309830152@gmail.com>
2024-05-25 11:07:17 +0530

    COMMIT

--- Web demo/react/demo/src/App.js
+++ Web demo/react/demo/src/App.js
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import './App.css';
+
 function App() {
   return (
-
     <div className="App">
-      This is reacts
+      This is react app
     </div>
   );
 }
